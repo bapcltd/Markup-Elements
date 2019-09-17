e7e43bb1e350a0ec238dcfb0ec37b784351837a3
Marv Blackwell <marv@bapc.ltd>
2019-09-17 12:04:35 +0100

    h3-h6

---
+++ src/Elements/H6.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* @author Marv Blackwell
+*/
+declare(strict_types=1);
+
+namespace BAPC\Html\Elements;
+
+/**
+* @psalm-type T1 = array<string, scalar|array<int, scalar>>
+* @psalm-type T2 = array<int, scalar|array{!element:string}>
+*
+* @template-extends AbstractElementFromAttributesAndContent<'h6', T1, T2>
+*/
+class H6 extends AbstractElementFromAttributesAndContent
+{
+	public static function ElementName() : string
+	{
+		return 'h6';
+	}
+}
